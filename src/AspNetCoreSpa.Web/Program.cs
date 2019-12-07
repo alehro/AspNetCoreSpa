5faaa28a3e106373164f6a1bbaa32fe1ec2b14b6
Alex Crome <alex.crome@bbconsult.co.uk>
2019-12-07 17:36:58 +0300

    runs on aws instance

--- src/AspNetCoreSpa.Web/Program.cs
+++ src/AspNetCoreSpa.Web/Program.cs
@@ -37,6 +37,8 @@ namespace AspNetCoreSpa.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
                   WebHost.CreateDefaultBuilder(args)
+                        //.UseUrls("http://*:5001;https://*:5002;")
+                        .UseUrls("http://*:5000;")
                         .ConfigureLogging((hostingContext, logging) =>
                         {
                             logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
