5914d2cf7e8f088b5349b1fddd48ba5230fe8390
Alex Crome <alex.crome@bbconsult.co.uk>
2020-01-02 14:15:35 +0300

    Mobile tree released. Note, it works in mobile browser but  unfortunately not in Web Widget as originally intended.

---
+++ TODO.md
@@ -0,0 +1,10 @@
+
+### Todo
++ Mobile tree
+	- drag and drop to rearrange nodes
+
+### Done
++ Mobile tree
+	+ basic minimal set of operations
+	+ context menu
+	+ saves to browser local storage
--- src/AspNetCoreSpa.Web/ClientApp/_Site/MobileTree/MobileTree.ts
+++ src/AspNetCoreSpa.Web/ClientApp/_Site/MobileTree/MobileTree.ts
@@ -6,16 +6,21 @@ import { TreeModule } from 'primeng/tree';
 import { TreeNode, MenuItem } from 'primeng/api';
 
 
+type TreeNodeExt = TreeNode & { editing?: boolean };
+
 @Component({
     templateUrl: './MobileTree.html'
 })
 export class MobileTree implements OnInit {
-    treeData: TreeNode[];
+    treeData: TreeNodeExt[];
+    selectedNode: TreeNodeExt;
     menuItems: MenuItem[];
     constructor(private http: HttpClient) {
         this.menuItems = [
-            { label: 'Rename', icon: 'fa fa-edit', command: (event) => this.rename() },
-            { label: 'Delete', icon: 'fa fa-close', command: (event) => this.delete() }
+            { label: 'Rename', icon: 'fa fa-edit', command: (event) => this.rename(this.selectedNode) },
+            { label: 'Delete', icon: 'fa fa-close', command: (event) => this.delete(this.selectedNode) },
+            { label: 'Add Child', icon: 'fa fa-arrow-right', command: (event) => this.addChild(this.selectedNode) },
+            { label: 'Add Sibling', icon: 'fa fa-arrow-down', command: (event) => this.addSibling(this.selectedNode) }
         ];
         this.treeData = [
             {
@@ -27,28 +32,28 @@ export class MobileTree implements OnInit {
                 children: [
                     {
                         label: "Node11"
-                    },
-                    {
-                        label: "Node12",
-                        children: [
-                            {
-                                label: "Node121"
-                            },
-                            {
-                                label: "Node122"
-                            }
-                        ]
                     }
-                ]
-            },
-            {
-                label: "Node2",              
-                children: [
-                    {
-                        label: "Node21"
-                    }
+                    //{
+                    //    label: "Node12",
+                    //    children: [
+                    //        {
+                    //            label: "Node121"
+                    //        },
+                    //        {
+                    //            label: "Node122"
+                    //        }
+                    //    ]
+                    //}
                 ]
             }
+            //{
+            //    label: "Node2",              
+            //    children: [
+            //        {
+            //            label: "Node21"
+            //        }
+            //    ]
+            //}
         ];
     }
 
@@ -78,11 +83,36 @@ export class MobileTree implements OnInit {
         
         localStorage.setItem(this.storageName, toStore);
     }
-    rename() {
-        alert("TODO");
+    onShowMenu(event: any) {
+        console.log("show menu");
     }
-    delete() {
-        alert("TODO");
+    rename(node: TreeNodeExt) {
+        //alert(node.label);
+        node.editing = true;
+    }
+    getParentNodes(node: TreeNodeExt) {
+        return <TreeNodeExt[]>(node.parent && node.parent.children) || this.treeData;
+    }
+    delete(node: TreeNodeExt) {
+        let ar = this.getParentNodes(node);
+        _.remove(ar, (n1) => {
+            return n1 == node;
+        });
+        this.saveTree();
+    }
+    addChild(node: TreeNodeExt) {
+        node.children = node.children || [];
+        let ar = <TreeNodeExt[]>node.children;// this.getParentNodes(node);
+        ar.push({ label: "", editing: true});
+    }
+   
+    addSibling(node: TreeNodeExt) {
+        let ar = this.getParentNodes(node);
+        ar.push({ label: "", editing: true });
+    }
+    onNodeChange(node: TreeNodeExt) {
+        node.editing = false;
+        this.saveTree();
     }
 
 
